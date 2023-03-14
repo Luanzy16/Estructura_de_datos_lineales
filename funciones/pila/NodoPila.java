72ab7f5859437884c47a9c46e5ded630411c0e65
Luis Sánchez <114003971+Luanzy16@users.noreply.github.com>
2023-03-14 04:14:35 -0500

    Add files via upload

---
+++ funciones/pila/NodoPila.java
@@ -0,0 +1,25 @@
+public class NodoPila {
+    private int valor;
+    private NodoPila siguiente;
+
+    public NodoPila(int valor) {
+        this.valor = valor;
+        this.siguiente = null;
+    }
+
+    public int getValor() {
+        return valor;
+    }
+
+    public void setValor(int valor) {
+        this.valor = valor;
+    }
+
+    public NodoPila getSiguiente() {
+        return siguiente;
+    }
+
+    public void setSiguiente(NodoPila siguiente) {
+        this.siguiente = siguiente;
+    }
+}
---
+++ funciones/pila/PilaDinamica.java
@@ -0,0 +1,64 @@
+public class PilaDinamica {
+    private NodoPila tope;
+    private int tamano;
+
+    public PilaDinamica() {
+        this.tope = null;
+        this.tamano = 0;
+    }
+
+    public void push(int elemento) {
+        NodoPila nuevoNodo = new NodoPila(elemento);
+        if (tope == null) {
+            tope = nuevoNodo;
+        } else {
+            nuevoNodo.setSiguiente(tope);
+            tope = nuevoNodo;
+        }
+        tamano++;
+    }
+
+    public int pop() {
+        if (tope == null) {
+            System.out.println("La pila está vacía");
+            return -1;
+        } else {
+            int elemento = tope.getValor();
+            tope = tope.getSiguiente();
+            tamano--;
+            return elemento;
+        }
+    }
+
+    public int peek() {
+        if (tope == null) {
+            System.out.println("La pila está vacía");
+            return -1;
+        } else {
+            return tope.getValor();
+        }
+    }
+
+    public boolean empty() {
+        return tope == null;
+    }
+
+    public int search(int elemento) {
+        int posicion = -1;
+        NodoPila actual = tope;
+        int distancia = 1;
+        while (actual != null) {
+            if (actual.getValor() == elemento) {
+                posicion = distancia;
+                break;
+            }
+            actual = actual.getSiguiente();
+            distancia++;
+        }
+        return posicion;
+    }
+
+    public int size() {
+        return tamano;
+    }
+}
